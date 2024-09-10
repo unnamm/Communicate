61f2d2168e3ac95ac473f6dca97ae955dff5cb89
ljs <ljs01@jdetek.co.kr>
2024-09-10 09:09:18 +0900

    add plc

---
+++ Com/PLC/Melsec.cs
@@ -0,0 +1,30 @@
+﻿using Com.Common;
+
+namespace Com.PLC
+{
+    public class Melsec
+    {
+        private readonly ActUtlTypeLib.ActUtlType _plc = new();
+
+        public void Connect(int actLogicalStationNumber)
+        {
+            _plc.ActLogicalStationNumber = actLogicalStationNumber;
+
+            if (_plc.Open() != 0)
+            {
+                throw new Exception("connect fail");
+            }
+        }
+
+        public void Write(string address, int value)
+        {
+            _plc.SetDevice(address, value);
+        }
+        public int Read(string address)
+        {
+            _plc.GetDevice(address, out int value);
+
+            return value;
+        }
+    }
+}
